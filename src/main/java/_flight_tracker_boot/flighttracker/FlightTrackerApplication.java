dab452cfc5ec1242af2d1efac333d8712ddf170a
Sisahga Phimmasone <80082494+Sisahga@users.noreply.github.com>
2024-02-26 13:35:12 -0500

    initial commit

---
+++ src/main/java/_flight_tracker_boot/flighttracker/FlightTrackerApplication.java
@@ -0,0 +1,13 @@
+package _flight_tracker_boot.flighttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTrackerApplication.class, args);
+	}
+
+}
