b8178ca55a73fe33f1172bb19e959e0d0829a83b
Sisahga Phimmasone <80082494+Sisahga@users.noreply.github.com>
2024-02-26 22:54:51 -0500

    Added cross origin for front-end.

--- src/main/java/_flight_tracker_boot/flighttracker/FlightController.java
+++ src/main/java/_flight_tracker_boot/flighttracker/FlightController.java
@@ -1,8 +1,6 @@
 package _flight_tracker_boot.flighttracker;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,9 +8,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/flights")
 
 public class FlightController {
